97ccae1d61dd806107b7e61df1afb1732df72790
pr3847 <manoharreddyndl988@gmail.com>
2024-02-01 11:15:53 +0530

    using switch instead if

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("Welcome to the employee wage");
-        int is_PartTime=1;
-        int is_FullTime=2;
         int empHrW=20,empHr=0,total;
-        double emp= Math.floor(Math.random()*10)%3;
-        if(emp==is_PartTime){
-            empHr=4;
-        }else if(emp==is_FullTime){
-            empHr=8;
+        int emp= (int)Math.floor(Math.random()*10)%3;
+        switch (emp){
+            case 1:
+                empHr=4;
+                break;
+            case 2:
+                empHr=8;
+                break;
+            default:
+                empHr=0;
+
         }
         total=empHrW*empHr;
         System.out.println("Employee today wage is: "+total);
