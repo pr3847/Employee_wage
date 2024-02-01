0bbd0ad4944f588c5adf52b537db92ea08f18c94
pr3847 <manoharreddyndl988@gmail.com>
2024-02-01 10:49:10 +0530

    Employee present

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the employee wage");
+        int is_present=1;
+        double emp= Math.floor(Math.random()*10)%2;
+        if(emp==is_present){
+            System.out.println("Employee is present");
+        }else{
+            System.out.println("Employee is absent");
+        }
     }
 }
