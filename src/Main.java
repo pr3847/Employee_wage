57a5fb32ee3dc7b0a7a5076f7771b05a676e947f
pr3847 <manoharreddyndl988@gmail.com>
2024-02-01 11:05:42 +0530

    adding parttym emplo

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to the employee wage");
-        int is_present=1;
+        int is_PartTime=1;
+        int is_FullTime=2;
         int empHrW=20,empHr=0,total;
-        double emp= Math.floor(Math.random()*10)%2;
-        if(emp==is_present){
+        double emp= Math.floor(Math.random()*10)%3;
+        if(emp==is_PartTime){
+            empHr=4;
+        }else if(emp==is_FullTime){
             empHr=8;
         }
         total=empHrW*empHr;
