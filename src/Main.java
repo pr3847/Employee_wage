131737e4d8b96d2fd582b8f01494c161515e3c5a
pr3847 <manoharreddyndl988@gmail.com>
2024-02-02 10:29:50 +0530

    save wage

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,12 @@
 import java.util.*;
 public class Main {
-    public static final int is_partTime=1;
-    public static final int is_fullTime=2;
-    public static final int empHrW=20;
-    public static final int no_of_days=20;
-    public static void calculateWage(String cmpnyname){
-        System.out.println("Company "+cmpnyname);
-        int empHr=0,total=0,totaldays=0,totalempHr=0;
 
-        while(totalempHr<=100 && totaldays<no_of_days) {
-            totaldays++;
-            int emp = (int) Math.floor(Math.random() * 10) % 3;
-            switch (emp) {
-                case is_partTime:
-                    empHr = 4;
-                    break;
-                case is_fullTime:
-                    empHr = 8;
-                    break;
-                default:
-                    empHr = 0;
-
-            }
-            totalempHr+=empHr;
-            System.out.println("Day:"+totaldays+" hours:"+empHr);
-        }
-        total=totalempHr*empHrW;
-        System.out.println("Total wage in "+cmpnyname+" for working "+totalempHr+"hrs in a month "+total);
-    }
     public static void main(String[] args) {
         System.out.println("Welcome to the employee wage");
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter no of companies:");
-        int n= sc.nextInt();
-        for(int i=0;i<n;i++){
-            System.out.println("Enter the Company name: ");
-            String name= sc.next();
-            calculateWage(name);
-        }
+
+        Employeewager hp=new Employeewager("hp",20,100,20);
+        hp.calculateWage();
+        System.out.println(hp);
 
     }
 }
